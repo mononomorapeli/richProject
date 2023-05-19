46c5a535f78f1301ab91b79eecab1f08217a12d8
mononomorapeli <minkymorapeli@gmail.com>
2023-05-19 11:11:01 +0200

    Initial Commit

---
+++ RichProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichProject
+//
+//  Created by DA MAC M1 122 on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
